5e93aa31dac3e8ce8d7cd51f9b582f20e3598efe
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-09-17 12:31:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+class Stopwatch extends React.Component {
+    constructor(display) {
+        super(display);
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.prnt(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+    }
+
+    prnt() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.prnt();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    write() {
+        save(this.format(this.times));
+    }
+
+    render() {
+    	return <div class={this.display}></div>;
+    }
+
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+function save(timer) {
+    let res = document.createElement('li');
+    res.innerText = timer;
+
+    let tab = document.querySelector('.results');
+    tab.insertBefore(res, tab.childNodes[0])
+}
+
+/*const stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'));*/
+
+let stopwatch = React.createElement(Stopwatch);
+ReactDOM.render(stopwatch, document.querySelector('.stopwatch'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.reset());
+
+let writeButton = document.getElementById('write');
+writeButton.addEventListener('click', () => stopwatch.write());
